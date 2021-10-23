462a7d7b43b3a54b4b598f5c62c20f231248758a
aDurkin <a.durkin@goodt.me>
2021-10-24 01:50:53 +0500

    add memory using and time of execution

--- src/edu/usue/zpie/dav/task02/Solution.java
+++ src/edu/usue/zpie/dav/task02/Solution.java
@@ -11,6 +11,8 @@ public class Solution {
         final int numberOfFields = scanner.nextInt();
         final List<Integer> priority;
         final ArrayList<Record> records;
+        long usedBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println(" Память " + (usedBytes / 1048576) + " мб");
         priority = IntStream.range(0, numberOfFields).mapToObj(i -> scanner.nextInt()).collect(Collectors.toList());
         records = IntStream.range(0, amountRecords).mapToObj(i -> scanner.nextLine())
                 .map(row -> new Record(row.substring(0, row.indexOf(" ")),
@@ -18,9 +20,14 @@ public class Solution {
                         .map(Integer::parseInt)
                         .collect(Collectors.toList())))
                 .collect(Collectors.toCollection(ArrayList::new));
+        long start = System.currentTimeMillis();
         Comparator<Record> multiComparator = new CompByPriority(priority);
         records.sort(multiComparator);
         records.forEach(m -> System.out.println(m.getName()));
+        long stop = System.currentTimeMillis();
+        usedBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println(" Память " + (usedBytes / 1048576) + " мб");
+        System.out.println(" Время выполнения " + (stop - start) + " мс");
     }
 
 }
